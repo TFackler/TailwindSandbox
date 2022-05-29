b7c36b8dd25f06c10c49903765dde63d0bff7ff8
TFackler <38538728+TFackler@users.noreply.github.com>
2022-05-29 22:09:51 +0200

    :sparkles: Add top graphic and basic text

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        forestDarkGreen: '#042522',
+        forestGreen: '#337930',
+        forestLightGreen: '#68ae61',
+        forestSupLightGreen: '#9ee694'
+      },
+      fontFamily: {
+        'josefin-sans': ['"Josefin Sans"'],
+        'work-sans': ['"Work Sans"']
+      }
+    },
   },
   plugins: [],
 }
